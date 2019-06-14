525b981a6cb90301d8db75e8232b916d16809b06
Chris Evans <chris@stash.global>
2019-06-14 16:26:44 -0500

    Updated gitignore, removed misc files not needed for distro

--- vendor/theseer/phpdox/src/collector/backend/SourceFileException.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types = 1);
-namespace TheSeer\phpDox\Collector\Backend;
-
-class SourceFileException extends \Exception {
-    public const BadEncoding = 1;
-
-    public const InvalidDataBytes = 2;
-}
